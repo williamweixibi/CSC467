c2af018deea84c0bba069d2940cc86edf5e47fc7
gmumar <gmumar@gmail.com>
2014-10-19 14:21:34 -0400

    Small fixes

--- Starter1/file.c
+++ Starter1/file.c
@@ -4,19 +4,23 @@ vec3 blabla;
 int db3
 bool boolenhere;
 104
-40.20
+0.1
+int40.20
 bla = -10 ;
 bla = -34.1;
 if (){
 
 }
 -.0
-02.
-010
+2.
+
+010e2
 int dp3(ivec3,ivec3);
 
 int y [ 3 ] ;
 
 int y[3];
 
-const_void_bla_bla_bla_________sfsfsdfsdfsdfsdfsdfsf_
+const_void_bla_bla_bla_
+
+010e
