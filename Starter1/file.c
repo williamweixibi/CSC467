57a729e5c7f7a651240aaf1eb505388e17f2dc7d
gmumar <gmumar@gmail.com>
2014-10-19 12:27:03 -0400

    fixes according to testcases

--- Starter1/file.c
+++ Starter1/file.c
@@ -3,7 +3,7 @@ int bla;
 vec3 blabla;
 int db3
 bool boolenhere;
-10
+104
 40.20
 bla = -10 ;
 bla = -34.1;
@@ -13,7 +13,10 @@ if (){
 -.0
 02.
 010
-int dp3
-(ivec3,ivec3);
+int dp3(ivec3,ivec3);
 
-const_void
+int y [ 3 ] ;
+
+int y[3];
+
+const_void_bla_bla_bla_________sfsfsdfsdfsdfsdfsdfsf_
